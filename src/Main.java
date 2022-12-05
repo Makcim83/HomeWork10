8489aaa28f30bbf55b59acf7b635e65fe2e7dd80
admin <mmd83@63.ru>
2022-12-05 21:16:13 +0400

    Домашнее задание - Тема "String" на проверку - выполнено 3/3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("HomeWork 10 Part 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
 
+        System.out.println("\nPart 2");
+        String smallCharsFullName = fullName;
+        String highCharsFullName = smallCharsFullName.toUpperCase();
+        System.out.println(highCharsFullName);
+
+        System.out.println("\nPart 3");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName);
+        fullName = fullName.replace("ё" , "е");
+        System.out.println(fullName);
     }
 }
